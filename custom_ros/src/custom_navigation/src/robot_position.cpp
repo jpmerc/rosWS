777e14a437fea3fb821be8eb0eee76f01514cf1c
JP <jp@jp-ThinkPad-T500.(none)>
2013-09-06 10:03:06 -0400

    addded repositories for launch files

--- custom_ros/src/custom_navigation/src/robot_position.cpp
+++ custom_ros/src/custom_navigation/src/robot_position.cpp
@@ -8,23 +8,23 @@
 using namespace std;
 
 
-
-ros::Publisher pointCloudPublisher;
-
-
-
-
-
 int main(int argc, char **argv)
 {
     //Init
     ros::init(argc, argv, "robot_position");
     ros::NodeHandle n;
+    ros::NodeHandle nh("~");
 
-    ros::Publisher pub1 = n.advertise<geometry_msgs::PoseWithCovarianceStamped>("/robot_1/amcl_pose", 100);
+    string robot_name = "";
+    nh.param("robot_name",robot_name, std::string("robot_0"));
+
+    ros::Publisher pub1 = n.advertise<geometry_msgs::PoseWithCovarianceStamped>("/" + robot_name +"/amcl_pose", 100);
 
     double x = 0.0;
     double y = 0.0;
+
+    //cout << "Robot " + robot_name + " amcl_pose simulation is started" << endl;
+    ROS_INFO("Robot %s amcl_pose simulation is started \n", robot_name.c_str());
 
     ros::Rate rate(20);
     while(ros::ok()){
@@ -34,11 +34,29 @@ int main(int argc, char **argv)
         msg.pose.pose.position.x = x;
         msg.pose.pose.position.y = y;
 
-        x+=0.005;
-        //y+=0.005;
+        if(robot_name == "robot_0"){
+           x+=0.005;
+        }
+        else if(robot_name == "robot_1"){
+            x-=0.005;
+        }
+        else if(robot_name == "robot_2"){
+            y+=0.005;
+        }
+        else if(robot_name == "robot_3"){
+            y-=0.005;
+        }
+        else if(robot_name == "robot_4"){
+            x+=0.005;
+            y+=0.005;
+        }
+        else if(robot_name == "robot_5"){
+            x-=0.005;
+            y-=0.005;
+        }
+
 
         pub1.publish(msg);
-        cout << "x = " << x << "  y = " << y << endl;
         ros::spinOnce();
         rate.sleep();
     }
