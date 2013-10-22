eebf4ce6327a404599b06c153e1d4efe63986ecc
turtlebot <turtlebot@optimus.(none)>
2013-10-22 15:32:35 -0400

    parameter update

--- custom_ros/src/custom_navigation/custom_navigation.sh
+++ custom_ros/src/custom_navigation/custom_navigation.sh
@@ -9,5 +9,5 @@ export TURTLEBOT_HUB=http://orleans.gel.ulaval.ca:6380
 source /opt/ros/groovy/setup.bash
 source ~/Devel/rosWS/custom_ros/devel/setup.sh
 export ROS_PACKAGE_PATH=~/Devel/rosWS/custom_ros:$ROS_PACKAGE_PATH
-#export HOKUYO_PORT=$(ls -l /dev/serial/by-id/usb-Hokuyo* | awk '{print $11}' | awk -F"/" '{print $3}')
-#echo ninja | sudo -S chmod a+rw /dev/ttyACM* /dev/ttyUSB* > /dev/null
+export HOKUYO_PORT=$(ls -l /dev/serial/by-id/usb-Hokuyo* | awk '{print $11}' | awk -F"/" '{print $3}')
+echo ninja | sudo -S chmod a+rw /dev/ttyACM* /dev/ttyUSB* > /dev/null
